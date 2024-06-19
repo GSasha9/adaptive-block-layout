5c81862c9f56b094bdc244a9b76f172e322acbcb
Alexandra <146391821+GSasha9@users.noreply.github.com>
2024-06-19 12:58:03 +0300

    Add files via upload
    added an overlay layer for the modal window

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 //вызываем форму авторизации
+//находим оверлей
+let overlay = document.querySelector('.modal');
 //находим форму
-let form = document.querySelector('#main--form');
+let form = document.querySelector('.main--form');
 //находим кнопку вызова формы
 let form_call = document.querySelector('#main--tilte_authorization--try_free');
 
 //назначаем слушатель события "клик" на кнопку вызова формы
 form_call.addEventListener('click', function(){
-    form.style.visibility = 'visible';
+    overlay.classList.add('modal--active');
 
 })
 
 //находим кнопку для закрытия формы, назначаем слушатель события "клик"
-let close_form = document.querySelector('#main--form>form>svg');
+let close_form = document.querySelector('.main--form>form>svg');
 close_form.addEventListener('click', function(){
-    form.style.visibility = 'hidden';
-    document.querySelector('#main--form>form').reset();
+    overlay.classList.remove('modal--active');
+    document.querySelector('.main--form>form').reset();
 })
+
+
+//закрытие модального окна по клику вне его контентной области
+overlay.onmousedown = function (e) {
+    let target = e.target;
+    let modalContent = overlay.getElementsByClassName('main--form')[0];
+    if (e.target.closest('.' + modalContent.className) === null) {
+      this.classList.remove('modal--active');
+      tagBody.classList.remove('hidden');
+    }
+  };
+
 
 //валидация формы
 window.addEventListener("DOMContentLoaded", function(){
-    document.querySelector("#main--form>form").addEventListener("submit", validateEnterForm);
+    document.querySelector(".main--form>form").addEventListener("submit", validateEnterForm);
 });
 
 function validateEnterForm(e){
@@ -29,15 +43,19 @@ function validateEnterForm(e){
     const mail_phone = isEmpty(e.target.mail_phone);
     const password = isEmpty(e.target.password);
     const remember = e.target.remember.checked;
+    console.log(mail_phone)
+    console.log(password)
     //если хотя бы одно из полей пустое - останавливаем отправку формы
     if(mail_phone || password){
         e.preventDefault();
+        return;
     }
     //если поля заполнены, создаем объект с введенными данными
     else{
         let AuthData = {
             login: e.target.mail_phone.value,
             password: e.target.password.value
+            
         }
         //если пользователь пожелал "запомниться на сайте", сохраняем его данные в формате JSON строки
         //так конечно не делают, это небезопасно, здесь это реализовано для того, чтобы показать работу чекбокса
@@ -51,11 +69,12 @@ function validateEnterForm(e){
                 alert('Ваш пароль уже сохранен на этом сайте');
             }
         }
+        overlay.classList.remove('modal--active');
     }
    
     //очищаем и скрываем форму
-    document.querySelector('#main--form>form').reset();
-    form.style.visibility = 'hidden';
+    document.querySelector('.main--form>form').reset();
+    
 };
 
 //проверяем не пустые ли поля
